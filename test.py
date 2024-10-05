762c1271b239b69c20423e36c4157a5b1ece2006
Leo Cameron <therussianbluecat@hotmail.com>
2024-10-04 20:00:00 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+# This is just a test that im making.
+
+ans = input("do you want to go to the home page?")
+if ans == "yes":
+    print("sorry, link unavailable.")
+
+elif ans == "no":
+    print("okay, exiting now")
+
+else:
+    print("that is not a valid answer.")
