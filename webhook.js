220e4b2559d3ecc004baa90e2f461f9d44a75bbc
Leo C <therussianbluecat@hotmail.com>
2024-10-26 21:37:01 -0400

    edit index and added webhook

---
+++ webhook.js
@@ -0,0 +1,9 @@
+fetch('https://discordapp.com/api/webhooks/1299906560591659058/qzoQ5kW3tsNd3zx2EreJi8gOJvPA5Vy3gfs8r73AgyP4DVCJkKEiM0NiPj22uIhi7lsS', {
+    method: 'POST', headers: {
+        'Content-Type': 'application/json'
+    }, body: JSON.stringify({
+        embeds: [{
+            title: "MBFDILC0114's Website", description: 'A website of mine', url: 'https://mbfdilc0114.github.io'
+        }]
+    })
+})
