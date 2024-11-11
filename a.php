11eef33d34748fa1bf7dc5d0edc428df01d9e995
Leo C <therussianbluecat@hotmail.com>
2024-11-11 15:09:01 -0500

    4 changes

--- a.php
+++
@@ -1,4 +0,0 @@
-$url = $_GET['url'];
-$file = file_get_contents($url);
-header("content-disposition: attachment; filename="". basename($url). """);
-echo $file;
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Test</title>
+    </head>
+</html>
+
+<body>
+    <h1>PHP on Github Pages Test</h1>
+    <p>i'm seeing if github pages support index.php</p>
+</body>
